6b6479045b54a3828ac80cc1fd8d3e0d81f107a4
Alex Mc'key <mckey2002@yandex.ru>
2021-12-02 15:46:15 +0300

    Refactor Day02 Scala3 solution

--- AoC_Scala3/src/day02/day02.scala
+++ AoC_Scala3/src/day02/day02.scala
@@ -3,29 +3,24 @@ package day02
 import AoC_Lib.*
 
 object day02 {
-  val cmds: Seq[(String, Int)] = inputStrs("input02.txt")
+  val cmds: Seq[Pos] = inputStrs("input02.txt")
     .map(_.split(' '))
-    .map{ a => a(0) -> a(1).toInt}
-    .toList
+    .collect{ case Array(a1,a2) => a1 -> a2.toInt match {
+      case ("forward",i) => Pos(i,0)
+      case ("up",i) => Pos(0,-i)
+      case ("down",i) => Pos(0,i)
+      case _ => Pos.zero
+    }}.toList
 
   @main
   def main(): Unit = {
-    val res1 = cmds.foldRight(Pos.zero){
-      (cur, acc) => cur._1 match {
-        case "forward" => acc + Pos(cur._2,0)
-        case "up" => acc - Pos(0,cur._2)
-        case "down" => acc + Pos(0,cur._2)
-        case _ => acc
-      }
-    }
+    val res1 = cmds.foldLeft(Pos.zero)(_+_)
     Console.println(res1.x * res1.y) // 2215080
 
     val res2 = cmds.foldLeft((0,Pos.zero)){
-      (acc, cur) => cur._1 match {
-        case "forward" => acc._1 -> (acc._2 + Pos(cur._2, acc._1 * cur._2))
-        case "up" => acc._1 - cur._2 -> acc._2
-        case "down" => acc._1 + cur._2 -> acc._2
-        case _ => acc
+      case ((aim, pos), cur) => cur.x match {
+        case 0 => aim + cur.y -> pos
+        case _ => aim -> (pos + Pos(cur.x, cur.x * aim))
       }
     }
     Console.println(res2._2.x * res2._2.y) // 1864715580
