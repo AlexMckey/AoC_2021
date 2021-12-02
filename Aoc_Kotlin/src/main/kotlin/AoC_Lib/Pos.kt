eecf642270049e66625cebdf91de316d96afaf7d
Alex Mc'key <mckey2002@yandex.ru>
2021-12-02 15:10:45 +0300

    Add Day02 Kotlin solution - Complete

--- Aoc_Kotlin/src/main/kotlin/AoC_Lib/Pos.kt
+++ Aoc_Kotlin/src/main/kotlin/AoC_Lib/Pos.kt
@@ -1,3 +1,28 @@
 package AoC_Lib
 
-data class Pos()
+import kotlin.math.abs
+
+data class Pos(var x: Int = 0, var y: Int = 0) {
+    override fun toString(): String = "[x:$x,y:$y]"
+    operator fun plus(other: Pos) = Pos(other.x + x, other.y + y)
+    operator fun plus(other: Pair<Int, Int>) = Pos(other.first + x, other.second + y)
+    operator fun minus(other: Pos) = Pos(other.x - x, other.y - y)
+    operator fun plusAssign(other: Pos) = run { x += other.x; y += other.y }
+    operator fun minusAssign(other: Pos) = run { x -= other.x; y -= other.y }
+    operator fun unaryMinus() = Pos(-x, -y)
+    operator fun times(other: Int) = Pos(x * other, y * other)
+    operator fun times(other: Pos) = Pos(x * other.x, y * other.y)
+    operator fun timesAssign(other: Int) = run { x *= other; y *= other }
+
+    fun manhattanDistance(p2: Pos = Zero): Int {
+        return abs(this.x - p2.y) + abs(this.y - p2.y)
+    }
+
+    companion object {
+        fun toPos(p: Pair<Int,Int>): Pos = Pos(p.first, p.second)
+        val Zero = Pos(0,0)
+        val Start = Zero
+    }
+}
+
+fun Pos.toPair(): Pair<Int,Int> = this.x to this.y
