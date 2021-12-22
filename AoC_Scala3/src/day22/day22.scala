3e68ed490910c58d8ad63876f227d096ef3a6ad5
Alex Mc'key <mckey2002@yandex.ru>
2021-12-22 23:10:08 +0300

    Minor changes in Day22 in Scala3 solution (optimization, remove slow - by count each point solution)

--- AoC_Scala3/src/day22/day22.scala
+++ AoC_Scala3/src/day22/day22.scala
@@ -11,36 +11,38 @@ object day22 {
     def intersect(that: ReactorArea): Option[ReactorArea] = {
       val intersectMin = min max that.min
       val intersectMax = max min that.max
-      val newState = (state,that.state) match
-        case ("on","on") => "off"
-        case ("off","off") => "on"
-        case ("on","off") => "off"
-        case ("off","on") => "on"
+      val newState = state match
+        case "on" => "off"
+        case "off" => "on"
       if intersectMin <= intersectMax
         then Some(ReactorArea(newState , intersectMin, intersectMax))
         else None
     }
+    def contains(that: ReactorArea): Boolean = (this intersect that).isEmpty
     def size: Long = (if state == "on" then 1L else -1L) *
       (max.x - min.x + 1) * (max.y - min.y + 1) * (max.z - min.z + 1)
   }
 
   def parseReactors(ss: Seq[String], limit: Int = Int.MaxValue): Seq[ReactorArea] = ss.map {
     case r(state, x1, x2, y1, y2, z1, z2) =>
-      ReactorArea(state, Pos3D(-limit max x1.toInt, -limit max y1.toInt, -limit max z1.toInt),
-                         Pos3D(limit min x2.toInt, limit min y2.toInt, limit min z2.toInt))
-  }
-  def operateReactors(reactors: Seq[ReactorArea]): Long = {
-    val all = scala.collection.mutable.Set.empty[(Int,Int,Int)]
-    reactors.foreach( r =>
-      (r.min.x to r.max.x).foreach( x =>
-        (r.min.y to r.max.y).foreach( y =>
-          (r.min.z to r.max.z).foreach( z =>
-            r.state match {
-              case "on" => all.add((x,y,z))
-              case "off" => all.remove((x,y,z))
-            }))))
-    all.toSet.size
-  }
+      ReactorArea(state, Pos3D(x1.toInt, y1.toInt, z1.toInt),
+                         Pos3D(x2.toInt, y2.toInt, z2.toInt))
+  }.filterNot(_ contains ReactorArea("off", Pos3D(-limit, -limit, -limit),
+                                            Pos3D(limit, limit, limit)))
+
+//  def operateReactors(reactors: Seq[ReactorArea]): Long = {
+//    val all = scala.collection.mutable.Set.empty[(Int,Int,Int)]
+//    reactors.foreach( r =>
+//      (r.min.x to r.max.x).foreach( x =>
+//        (r.min.y to r.max.y).foreach( y =>
+//          (r.min.z to r.max.z).foreach( z =>
+//            r.state match {
+//              case "on" => all.add((x,y,z))
+//              case "off" => all.remove((x,y,z))
+//            }))))
+//    all.toSet.size
+//  }
+
   def operateReactorsOpt(reactors: Seq[ReactorArea]): Long = {
     @tailrec
     def rec(reactors: Seq[ReactorArea], sectors: Seq[ReactorArea]): Seq[ReactorArea] =
@@ -48,19 +50,18 @@ object day22 {
       else
         val reactor = reactors.head
         val newSectors = (if reactor.state == "on" then sectors :+ reactor else sectors) ++
-          sectors.map(_ intersect reactor).collect{ case Some(s) => s }
+          sectors.flatMap(_ intersect reactor)
         rec(reactors.tail, newSectors)
     rec(reactors, Seq.empty).map(_.size).sum
   }
 
 
-  def part1: Long = operateReactors(parseReactors(inputStrs(day = 22), limit = 50))
-
+  def part1_opt: Long = operateReactorsOpt(parseReactors(inputStrs(day = 22), limit = 50))
   def part2: Long = operateReactorsOpt(parseReactors(inputStrs(day = 22)))
 
   @main
   def ReactorReboot(): Unit = {
-    Console.println(part1)     // 556501
+    Console.println(part1_opt) // 556501
     Console.println(part2)     // 1217140271559773
   }
 }
