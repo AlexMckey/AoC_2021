12ab3a0e82ad3dd422d49addb67f2daaf315b7a0
Alex Mc'key <mckey2002@yandex.ru>
2021-12-22 19:42:12 +0300

    Minor changes in Day22 in Scala3 solution

--- AoC_Scala3/src/day22/day22.scala
+++ AoC_Scala3/src/day22/day22.scala
@@ -29,7 +29,7 @@ object day22 {
       ReactorArea(state, Pos3D(-limit max x1.toInt, -limit max y1.toInt, -limit max z1.toInt),
                          Pos3D(limit min x2.toInt, limit min y2.toInt, limit min z2.toInt))
   }
-  def operateReactors(reactors: Seq[ReactorArea]): Set[(Int,Int,Int)] = {
+  def operateReactors(reactors: Seq[ReactorArea]): Long = {
     val all = scala.collection.mutable.Set.empty[(Int,Int,Int)]
     reactors.foreach( r =>
       (r.min.x to r.max.x).foreach( x =>
@@ -39,7 +39,7 @@ object day22 {
               case "on" => all.add((x,y,z))
               case "off" => all.remove((x,y,z))
             }))))
-    all.toSet
+    all.toSet.size
   }
   def operateReactorsOpt(reactors: Seq[ReactorArea]): Long = {
     @tailrec
@@ -53,7 +53,8 @@ object day22 {
     rec(reactors, Seq.empty).map(_.size).sum
   }
 
-  def part1: Int = operateReactors(parseReactors(inputStrs(day = 22),50)).size
+
+  def part1: Long = operateReactors(parseReactors(inputStrs(day = 22), limit = 50))
 
   def part2: Long = operateReactorsOpt(parseReactors(inputStrs(day = 22)))
 
